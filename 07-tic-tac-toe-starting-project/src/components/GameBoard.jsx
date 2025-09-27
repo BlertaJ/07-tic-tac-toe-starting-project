ad7ff56d22dc28c92ac1245da75050b13ed43014
Blerta Jashari <blerta.j@gjirafa.com>
2025-09-28 00:22:59 +0200

    Disable buttons conditionally

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -4,28 +4,14 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare, turns}) {
-  
+export default function GameBoard({ onSelectSquare, turns }) {
   let gameBoard = initialGameBoard;
-  
+
   for (let turn of turns) {
-    const {square, player } = turn;
+    const { square, player } = turn;
     const { row, col } = square;
     gameBoard[row][col] = player;
   }
-
-  // We are avoiding intersecting states
-  // const [activePlayerSymbol, setActivePlayerSymbol] = useState("X");
-  // const [gameBoard, setgameBoard] = useState(initialGameBoard);
-
-  // function handleSelectSquare(rowIndex, cellIndex) {
-  //   setgameBoard((prevGameBoard) => {
-  //     const updatedGameBoard = prevGameBoard.map(row => [...row]);
-  //     updatedGameBoard[rowIndex][cellIndex] = activePlayerSymbol
-  //     return updatedGameBoard;
-  //   });
-  //   onSelectSquare();
-  // }
 
   return (
     <ol id="game-board">
@@ -34,8 +20,11 @@ export default function GameBoard({onSelectSquare, turns}) {
           <ol>
             {row.map((playerSymbol, cellIndex) => (
               <li key={cellIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, cellIndex)}>
-                  {playerSymbol || ""}
+                <button
+                  onClick={() => onSelectSquare(rowIndex, cellIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
                 </button>
               </li>
             ))}
