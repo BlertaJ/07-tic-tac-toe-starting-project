dcf1a85c1d603dfa99d8bc30cab7465a5c3df6ae
Blerta Jashari <blerta.j@gjirafa.com>
2025-09-27 22:07:23 +0200

    Add imutable state

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,6 +7,16 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  contet[(gameBoard, setgameBoard)] = useState(initialGameBoard); //to manage and update the game board state
+
+  function handleSelectSquare(rowIndex, cellIndex) {
+    setgameBoard((prevGameBoard) => {
+      const updatedGameBoard = [...prevGameBoard.map((row) => [...row])]; // Create a copy of the game board
+      updatedGameBoard[rowIndex][cellIndex] = "X";
+      return updatedGameBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
       {initialGameBoard.map((row, rowIndex) => (
@@ -12,7 +24,7 @@ export default function GameBoard() {
           <ol>
             {row.map((playerSymbol, cellIndex) => (
               <li key={cellIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, cell)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
